a1c9d2ca55f6a9a27a3e436d3b1a4538673f2cc7
Mattias1011 <matjo680@student.liu.se>
2024-04-04 14:21:35 +0200

    login page frontend done v1

--- disctracker/src/App.vue
+++ disctracker/src/App.vue
@@ -13,36 +13,7 @@ import loginview from './views/loginview.vue';
 </template>
 
 <style scoped>
-@import 'tailwindcss/base';
-@import 'tailwindcss/components';
-@import 'tailwindcss/utilities';
 
 
 
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- disctracker/src/views/loginview.vue
+++ disctracker/src/views/loginview.vue
@@ -1,26 +1,52 @@
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center bg-blue-500">
-      <h1 class="text-5xl font-bold text-white mb-8">
-        Disctracker
-      </h1>  
-      <div class="bg-white p-8 rounded shadow-md w-80">
-        <h2 class="text-xl font-bold mb-4">Login</h2>
-        <form @submit.prevent="handleSubmit">
+    <div class="min-h-screen flex flex-col items-center justify-top bg-blue-500">
+      <img src="../assets/logo.png" alt="Disctracker" class="mb-8"> 
+      <div class="bg-white p-8 rounded shadow-md w-80 -mt-20">
+        <h2 class="text-xl font-bold mb-4">{{mode=='signin' ? 'Log in': 'Sign up'}}</h2>
+        <form v-if="mode === 'signin'" @submit.prevent="handleSignin">
           <div class="mb-4">
             <label for="username" class="block text-gray-700 font-bold mb-2">Username</label>
             <input type="text" v-model="username" id="username" name="username" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
           </div>
-          <div class="mb-6">
+          <div class="mb-4">
             <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
             <input type="password" v-model="password" id="password" name="password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
           </div>
-          <div class="flex items-center justify-between">
-            <div class="flex items-center"> 
-              <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Login</button>
-              <a href="signup" class="ml-6 text-blue-500">Sign up</a> 
-            </div>
+        </form>
+        <form v-if="mode === 'signup'" @submit.prevent="handleSignup">
+          <div class="mb-4">
+            <label for="username" class="block text-gray-700 font-bold mb-2">Username</label>
+            <input type="text" v-model="username" id="username" name="username" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+            <input type="email" v-model="email" id="email" name="email" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
+          </div>
+          <div class="mb-4">
+            <label for="gender" class="block text-gray-700 font-bold mb-2">Gender</label>
+            <select v-model="gender" id="gender" name="gender" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
+            <option value ="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+            </select>
+          </div>
+          <div class="mb-4">
+            <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
+            <input type="password" v-model="password" id="password" name="password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
+          </div>
+          <div class="mb-6">
+            <label for="repeatpsw" class="block text-gray-700 font-bold mb-2">Repeat password</label>
+            <input type="password" v-model="repeatpsw" id="repeatpsw" name="repeatpsw" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" required>
           </div>
         </form>
+          <div class="flex items-center justify-between">
+            <div class="flex items-center"> 
+              <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">{{mode=='signin' ? 'Log in': 'Sign up'}}</button>
+              <a href="signup" class="ml-6 text-blue-500" @click.prevent="toggleMode">{{mode=='signin' ? 'Sign up': 'Log in'}}</a> 
+            </div>
+          </div>
+        
       </div>
     </div>
   </template>
@@ -30,9 +56,22 @@
   
   <script>
   export default {
-    
+
+    data(){
+      return{
+        mode: 'signin'
+      };
+    },
     methods: {
-      handleSubmit() {
+      toggleMode(){
+        this.mode = this.mode === 'signin' ? 'signup': 'signin';
+      },
+      handleSignin() {
+        // Handle form submission here (e.g., send login request)
+        console.log('Username:', this.username);
+        console.log('Password:', this.password);
+      },
+      handleSignup() {
         // Handle form submission here (e.g., send login request)
         console.log('Username:', this.username);
         console.log('Password:', this.password);
