f64d67b415c3c953cf6a6109688a3f4967b2ba9a
wille <wilma154.student.liu.se>
2024-04-19 16:26:28 +0200

    styled dashboard + navbar done

--- disctracker/src/views/courses.vue
+++ disctracker/src/views/courses.vue
@@ -1,28 +1,61 @@
 <template>
    
-   <div class="flex justify-center items-top h-full w-full">
-        <div class="w-1/2 h-1/6 bg-blue-900 rounded-lg mt-20">
-            <h2 class="text-white text-3xl font-bold">Rydskogen Discgolf</h2>
-            <div class="grid grid-cols-9 mt-4">
+   <div class="flex flex-col justify-top items-center h-full w-full">
+        <div class="w-1/2 h-1/4 bg-sky-300 bg-opacity-50 rounded-lg mt-20">
+            <h2 class="text-white text-3xl font-bold ml-2">Rydskogen DGC</h2>
+            <div class="grid grid-cols-9 mt-4 mr-2 ml-2">
                 <template v-for="num in 9" :key="`box-${num}`">
                     <div class="box bg-green-500 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
                 </template>
             </div>
-            <div class="grid grid-cols-9">
+            <div class="grid grid-cols-9  mr-2 ml-2">
                 <template v-for="(num, index) in rydsParFront" :key="`box-${num}`">
-                    <div class="box bg-white text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
+                    <div class="box bg-blue-200 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
                 </template>
             </div>
       
-            <div class="grid grid-cols-9 mt-4">
+            <div class="grid grid-cols-9 mt-4  mr-2 ml-2">
                 <template v-for="num in 9" :key="`box-${num + 9}`">
                     <div class="box bg-green-500 text-black flex justify-center items-center rounded border border-black w-full">{{ num + 9 }}</div>
                 </template>
             </div>
-            <div class="grid grid-cols-9">
+            <div class="grid grid-cols-9 mb-2  mr-2 ml-2">
                 <template v-for="(num, index) in rydsParBack" :key="`box-${num }`">
-                    <div class="box bg-white text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
+                    <div class="box bg-blue-200 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
                 </template>
+            </div>
+            <div>
+                <button @click="startRound()" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm ml-2">Start new round</button>
+                
+            </div>
+            
+        </div>
+        <div class="w-1/2 h-1/4 bg-sky-300 bg-opacity-50 rounded-lg mt-20">
+            <h2 class="text-white text-3xl font-bold ml-2">Hammaren DiscGolfPark</h2>
+            <div class="grid grid-cols-9 mt-4 mr-2 ml-2">
+                <template v-for="num in 9" :key="`box-${num}`">
+                    <div class="box bg-green-500 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
+                </template>
+            </div>
+            <div class="grid grid-cols-9  mr-2 ml-2">
+                <template v-for="(num, index) in hammarenParFront" :key="`box-${num}`">
+                    <div class="box bg-blue-200 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
+                </template>
+            </div>
+      
+            <div class="grid grid-cols-9 mt-4  mr-2 ml-2">
+                <template v-for="num in 9" :key="`box-${num + 9}`">
+                    <div class="box bg-green-500 text-black flex justify-center items-center rounded border border-black w-full">{{ num + 9 }}</div>
+                </template>
+            </div>
+            <div class="grid grid-cols-9 mb-2  mr-2 ml-2">
+                <template v-for="(num, index) in hammarenParBack" :key="`box-${num }`">
+                    <div class="box bg-blue-200 text-black flex justify-center items-center rounded border border-black w-full">{{ num }}</div>
+                </template>
+            </div>
+            <div>
+                <button @click="startRound()" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm ml-2">Start new round</button>
+                
             </div>
             
         </div>
@@ -41,22 +74,17 @@
 
   const rydsParFront = [3, 4, 4, 3, 3, 3, 3, 5, 3];
   const rydsParBack = [3, 4, 3, 5, 3, 3, 3, 4, 3];
+  const hammarenParFront = [3, 3, 3, 3, 3, 3, 3, 3, 4];
+  const hammarenParBack = [3, 4, 3, 4, 3, 3, 3, 3, 3];
   // Define methods
-  const toggleMode = () => {
 
-  };
-
-  const handleSignin = () => {
-  
-  };
-
-  const handleSignup = () => {
-    
-  };
   onMounted(async () => {
   
  });
-
+ const startRound = () => {
+    alert("lets roll!");
+ };
+ 
  
 </script>
 
--- disctracker/src/views/profile.vue
+++ disctracker/src/views/profile.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="flex justify-center items-top h-full w-full">
-        <div class="w-1/6 h-1/6 bg-blue-900 rounded-lg mt-20">
-            <p>Your previous round</p>
+        <div class="w-1/6 h-1/6 bg-blue-600 rounded-lg mt-20 mr-10">
+            <h2 class="text-white text-3xl font-bold">Your previous round</h2>
+        </div>
+        <div class="w-1/6 h-1/6 bg-blue-600 rounded-lg mt-20 mr-10">
+            <h2 class="text-white text-3xl font-bold">Your best round</h2>
+        </div>
+        <div class="w-1/6 h-1/3 bg-blue-600 rounded-lg mt-20 mr-10">
+            <h2 class="text-white text-3xl font-bold">Your averages</h2>
         </div>
            
     </div>   
@@ -18,17 +24,9 @@
   const username = ref('');
 
   // Define methods
-  const toggleMode = () => {
-
-  };
-
-  const handleSignin = () => {
-  
-  };
-
-  const handleSignup = () => {
+ 
     
-  };
+
   onMounted(async () => {
   
  });
