b5c90077a64f4e5939b976342feeaac5589e60a8
wille <wilma154.student.liu.se>
2024-05-07 14:05:22 +0200

    added friendslist

--- disctracker/src/views/friends.vue
+++ disctracker/src/views/friends.vue
@@ -7,12 +7,19 @@
                 <input v-model="playername" type="text" class="mt-5 ml-2 p-2 border border-gray-300 rounded-lg" placeholder="Search for a player">
                 <button @click.prevent="addFriend" class="ml-2 p-2 bg-blue-500 text-white rounded-lg">
                     Add Friend
-                    <span v-if="friendStatus === 'success'" class="text-green-500 ml-2">&#10004;</span>
-                    <span v-else-if="friendStatus === 'error'" class="text-red-500 ml-2">&#10008;</span>
                 </button>
+                <p v-if="friendStatus === 'success'" class="text-green-500 mt-2 ml-2">Friend added successfully!</p>
+                <p v-else-if="friendStatus === 'nouser'" class="text-red-500 mt-2 ml-2">Invalid playername. Please try again</p>
+                <p v-else-if="friendStatus === 'friends'" class="text-blue-500 mt-2 ml-2">You are already friends</p>
              </div>
              
              
+         </div>
+         <div class="w-1/2 h-1/4 bg-slate-900 bg-opacity-90 rounded-lg mt-20 flex flex-col items-center">
+            <h2 class="text-white text-md md:text-xl lg:text-3xl font-bold ml-2">Your Friends</h2>
+            <ul>
+                <li v-for="(friend, index) in friends" :key="index" class="text-white">{{ friend }}</li>
+            </ul>
          </div>
      </div>
  </template>
@@ -24,6 +31,7 @@
    import { ref, onMounted} from 'vue'
    const playername = ref('');
    const friendStatus = ref('');
+   const friends = ref([]);
    const addFriend = () => {
     const data = {
         playername: playername.value
@@ -43,9 +51,15 @@
             // Handle successful response from the backend
             console.log('Friend added successfully');
             friendStatus.value = 'success';
+            getFriends();
             return response.json();
         } else if (response.status === 409) {
+            return response.json().then(json => {
+                console.log(json);
+                friendStatus.value = json.message === 'nouser' ? 'nouser' : 'friends';
             throw new Error('User not found or already added as friend');
+            });
+            
         } else {
             throw new Error('Failed to add friend');
         }
@@ -55,12 +69,37 @@
     })
     .catch(error => {
         console.error('Error adding friend:', error.message);
-        friendStatus.value = 'error';
+        
+    });
+};
+const getFriends = () => {
+    fetch('http://127.0.0.1:5000/getfriends', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }else{
+            throw new error('Failed to get friends');
+        }
+    })
+    .then(data => {
+        if(data.friends){
+            friends.value = data.friends;
+        }
+    })
+    .catch(error => {
+        console.error('Error adding friends:', error.message);
+        
     });
 };
  
    onMounted(async () => {
-   
+    getFriends();
   });
   
   
