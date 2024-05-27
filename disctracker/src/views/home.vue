5d24d7e87824e8a2a943e17de28258a9f6d75f2d
wille <wilma154.student.liu.se>
2024-05-27 16:59:12 +0200

    fixed homepage so it welcomes th user

--- disctracker/src/views/home.vue
+++ disctracker/src/views/home.vue
@@ -1,31 +1,15 @@
 <template>
-    <div class="flex flex-wrap relative justify-center items-top w-full mt-44">
-        <div class="lg:w-8/12 w-11/12 lg:mx-2 my-2 h-96">
-            <div class="relative bg-slate-900 bg-opacity-90 rounded-lg p-4 h-full">
-                <p class="text-white">Welcome back {{ username }}</p>
-                <div class="w-1/2 h-80 absolute right-5 bg-slate-700 rounded-lg">
-                    <div :style="{ height: `${dynamicHeight-60}px` }" class="flex flex-col rounded-lg absolute bottom-2 right-[11%] bg-slate-500 justify-center w-[13%] ">
-                        <p class="text-white self-center">129</p>
-                     </div>
-                    <div :style="{ height: `${dynamicHeight-30}px`}" class="flex flex-col rounded-lg absolute bottom-2 right-[29%] bg-slate-500 justify-center w-[13%]">
-                        <p class="text-white self-center">100</p>
-                    </div>
-                    <div :style="{ height: `${dynamicHeight}px`}" class="flex flex-col rounded-lg absolute bottom-2 right-[47%] bg-slate-500 justify-center w-[13%]">
-                        <p class="text-white self-center">82</p>
-                    </div>
-                    <div :style="{ height: `${dynamicHeight-60}px` }" class="flex flex-col rounded-lg absolute bottom-2 right-[65%] bg-slate-500 justify-center w-[13%]">
-                        <p class="text-white self-center">71</p>
-                     </div>
-                    <div :style="{ height: `${dynamicHeight-30}px`}" class="flex flex-col rounded-lg absolute bottom-2 right-[82%] bg-slate-500 justify-center w-[13%]">
-                        <p class="text-white self-center">24</p>
-                    </div>
-                </div>
+    <div class="flex flex-col relative justify-top items-center w-full h-full mt-44">
+        <div class="lg:w-8/12 w-11/12 lg:mx-2 mt-2 h-1/6 mb-10">
+            <div class="relative bg-slate-900 bg-opacity-90 rounded-lg p-4 h-full flex justify-center items-center">
+                <p class="text-white text-md md:text-xl lg:text-3xl font-bold">Welcome back {{ username }}!</p>
+                
             </div>
         </div>
-        <div class="relative justify-center lg:w-8/12 w-11/12 lg:mx-2 my-2 h-60">
+        <div class="relative lg:w-8/12 w-11/12 lg:mx-2 mb-2 h-1/3">
             <div class="flex flex-col bg-slate-900 bg-opacity-90 rounded-lg p-4 h-full justify-center items-center hover:bg-slate-800 hover:bg-opacity-90"
                         @click="$emit('myEvent')">
-                <p class="text-white">Start new game</p>
+                <p class="text-white text-md md:text-xl lg:text-3xl font-bold">Start new game</p>
             </div>
         </div>
     </div>
@@ -36,11 +20,12 @@
   import { ref, onMounted} from 'vue'
 
   // Define reactive variables
-  const username = ref('user1');
+  const username = ref('');
   const dynamicHeight = ref('300'); // Initial height value
   // Define methods
   
     onMounted(async () => {
+        getUser();
         fetch('http://localhost:5000/dashboard', {
         method: 'GET',
         headers: {
@@ -60,7 +45,35 @@
             console.error('Failed to check token:', error);
         });
     });
- 
+    const getUser = () => {
+    fetch('http://127.0.0.1:5000/getuser', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }else{
+            throw new Error('Failed to get user');
+        }
+    })
+    .then(data => {
+        console.log(data);
+        if(data.username){
+            username.value = data.username;
+            
+        }
+    })
+    .catch(error => {
+        console.error('Error finding user:', error.message);
+        
+    });
+    
+    
+}; 
 </script>
 
 <style>
